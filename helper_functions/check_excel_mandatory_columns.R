4a7d60ff52a4a7a09a7ab69620fd356f4991f028
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2026-03-17 12:39:30 +0100

    Update check_excel_mandatory_columns.R

--- helper_functions/check_excel_mandatory_columns.R
+++ helper_functions/check_excel_mandatory_columns.R
@@ -2,7 +2,7 @@
 check_excel_mandatory_columns <- function(folder_path, cols) {
   # Ensure the folder exists
   if (!dir.exists(folder_path)) {
-    return(data.table(Folder = basename(folder_path), Result = "Invalid", Comment = "Concept folder does not exist in the main app directory."))
+    return(data.table(Folder = basename(folder_path), Result = "Invalid", Comment = "Folder does not exist."))
   }
   
   # Get the folder name
@@ -11,7 +11,7 @@ check_excel_mandatory_columns <- function(folder_path, cols) {
   # Extract the first 3 elements of the folder name
   folder_parts <- strsplit(folder_name, "_")[[1]]
   if (length(folder_parts) < 3) {
-    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "Concept folder name does not have at least three elements."))
+    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "Folder name does not have at least three elements."))
   }
   expected_filename <- paste(folder_parts[1:3], collapse = "_")
   
@@ -19,7 +19,7 @@ check_excel_mandatory_columns <- function(folder_path, cols) {
   excel_files <- list.files(folder_path, pattern = "\\.xlsx$|\\.xls$", full.names = TRUE)
   
   if (length(excel_files) == 0) {
-    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "No Concept Excel file found in the folder."))
+    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "No Excel file found in the folder."))
   }
   
   # Find the Excel file that matches the expected name
@@ -27,7 +27,7 @@ check_excel_mandatory_columns <- function(folder_path, cols) {
                                   basename(excel_files) == paste0(expected_filename, ".xls")]
   
   if (length(matching_files) == 0) {
-    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "No matching Concept Excel file found."))
+    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "No matching Excel file found."))
   }
   
   # Load the Excel file
@@ -37,12 +37,12 @@ check_excel_mandatory_columns <- function(folder_path, cols) {
   sheet_names <- tryCatch(excel_sheets(excel_file), error = function(e) return(NULL))
   
   if (is.null(sheet_names)) {
-    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "Error reading Concept Excel file."))
+    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "Error reading Excel file."))
   }
   
   # Check if the sheet with the same name as the Excel file exists
   if (!(expected_filename %in% sheet_names)) {
-    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "Sheet with expected name not found in the Concept Excel file."))
+    return(data.table(Folder = folder_name, Result = "Invalid", Comment = "Sheet with expected name not found in the Excel file."))
   }
   
   # Load the sheet
@@ -64,10 +64,10 @@ check_excel_mandatory_columns <- function(folder_path, cols) {
   
   if (length(missing_columns) > 0) {
     return(data.table(Folder = folder_name, Result = "Invalid", 
-                      Comment = paste("Missing mandatory columns:", paste(missing_columns, collapse = ", "), ". Review requirements and add the necessary column names.")))
+                      Comment = paste("Missing mandatory columns:", paste(missing_columns, collapse = ", "))))
   }
   
   # If all checks pass
-  return(data.table(Folder = folder_name, Result = "Valid", Comment = "All Excel files in concepts contain the required columns."))
+  return(data.table(Folder = folder_name, Result = "Valid", Comment = "All checks passed successfully."))
 }
 
